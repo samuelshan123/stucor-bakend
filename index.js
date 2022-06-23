bbc4f0fe1ec5cb80300ea11f12c56da412fef92c
samuelshan123 <samuelrajraje@retroacto.com>
2022-06-23 14:35:50 +0530

    minor changes

--- index.js
+++ index.js
@@ -110,23 +110,25 @@ app.post('/getrequests',async (req,res)=>{
 // Approve or enquire forms
 
 app.post('/action',async (req,res)=>{
-  console.log(req.body);
-
+  console.log("body",req.body);
+  var sql_query,values
   if (req.body.role=== 'in-charge') {
-    var sql_query = `UPDATE forms SET status=?,incharge_id=?,iactioned_at=? WHERE user_id=? and request_id=?`
-    var values = [req.body.status,req.body.incharge_id,req.body.iactioned_at,req.body.id,req.body.request_id]
+     sql_query = `UPDATE forms SET status=?,incharge_id=?,iactioned_at=? WHERE user_id=? and request_id=?`
+     values = [req.body.status,req.body.incharge_id,req.body.iactioned_at,req.body.id,req.body.request_id]
   }
   else if(req.body.role === 'hod') {
-    var sql_query = `UPDATE forms SET status=?,hod_id=?,hactioned_at=? WHERE user_id=? and request_id=?`
-    var values = [req.body.status,req.body.hod_id,req.body.hactioned_at,req.body.id,req.body.request_id]
+     sql_query = `UPDATE forms SET status=?,hod_id=?,hactioned_at=? WHERE user_id=? and request_id=?`
+     values = [req.body.status,req.body.hod_id,req.body.hactioned_at,req.body.id,req.body.request_id]
   }
   else if(req.body.role === 'principal') {
-    var sql_query = `UPDATE forms SET status=?,pactioned_at=? principal_id=? WHERE user_id=? and request_id=?`
-    var values = [req.body.status,req.body.pactioned_at,req.body.principal_id,req.body.id,req.body.request_id]
+    console.log("body",req.body);
+
+     sql_query = "UPDATE forms SET status=?,pactioned_at=? ,principal_id=? WHERE user_id=? and request_id=?"
+     values = [req.body.status,req.body.pactioned_at,req.body.principal_id,req.body.id,req.body.request_id]
   }
  
   await db.query(sql_query,values,(err,result)=>{
-    console.log(result);
+    console.log("resr",result);
     if(err) throw err;
     if (result.affectedRows>0) {
       res.send({status:"success"})  
@@ -194,7 +196,7 @@ app.post('/hodApproved',async (req,res)=>{
 app.post('/completed',async (req,res)=>{
   console.log(req.body);
   if (req.body.role==='student') {
-    var sql_query = `SELECT stucor.staffs.name,stucor.forms.status,stucor.forms.hactioned_at,stucor.forms.description,stucor.forms.form_type,stucor.forms.requested_at FROM stucor.forms left join stucor.students on stucor.students.id =stucor.forms.user_id left join stucor.staffs on stucor.forms.hod_id= stucor.staffs.id WHERE stucor.students.id=? and stucor.forms.status='AFH'  and stucor.forms.form_type ='leave form' order by stucor.forms.request_id desc`
+    var sql_query = `SELECT stucor.staffs.name,stucor.forms.status,stucor.forms.hactioned_at,stucor.forms.description,stucor.forms.form_type,stucor.forms.requested_at FROM stucor.forms left join stucor.students on stucor.students.id =stucor.forms.user_id left join stucor.staffs on stucor.forms.hod_id= stucor.staffs.id WHERE stucor.students.id=? and stucor.forms.status='AFH' and stucor.forms.form_type ='leave form' order by stucor.forms.request_id desc`
   var values = [req.body.id] 
   }
   else if(req.body.role==='hod') {
@@ -340,7 +342,7 @@ console.log('hitted');
 
 //check out form {}
 app.post('/checkoutForm',async (req,res)=>{
-  console.log(req.body);
+  console.log(req.body);  
   var sql_query = `UPDATE stucor.forms SET forms.status='CO', forms.security_id=? ,forms.checked_at=? WHERE forms.request_id=?`
   var values = [req.body.security_id,req.body.checked_at,req.body.request_id]
   await db.query(sql_query,values,(err,result)=>{
@@ -366,6 +368,10 @@ app.post('/checkedOutForm',async (req,res)=>{
     var sql_query = `SELECT * FROM stucor.forms left join stucor.students on stucor.students.id =stucor.forms.user_id where  stucor.forms.status='CO' and stucor.students.department=? order by stucor.forms.request_id desc`
     var values = [req.body.department]
 
+  }
+  else if(req.body.role==='student'){
+    var sql_query = `SELECT * FROM stucor.forms left join stucor.students on stucor.students.id =stucor.forms.user_id where  stucor.forms.status='CO' and stucor.students.id=? order by stucor.forms.request_id desc`
+    var values = [req.body.id]
   }
   else{
     var sql_query=`SELECT * FROM stucor.forms left join stucor.students on stucor.students.id =stucor.forms.user_id 
